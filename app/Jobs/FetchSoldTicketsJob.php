2a1c93d782b1bc3dc4456123ce0ba207dc909e42
Ghale <ghale.madayag@gmail.com>
2025-03-17 15:13:44 +0800

    v1.20.3

--- app/Jobs/FetchSoldTicketsJob.php
+++ app/Jobs/FetchSoldTicketsJob.php
@@ -62,6 +62,7 @@ class FetchSoldTicketsJob implements ShouldQueue
             foreach ($data['rows'] as $item) {
                 $eventId = $item['eventId'];
                 $quantity = $item['quantity'];
+                
                 $profit = $item['profitMargin'];
                 $invoiceDate = Carbon::parse($item['invoiceDate']);
 
@@ -73,7 +74,9 @@ class FetchSoldTicketsJob implements ShouldQueue
                         'venue' => $item['event']['venue']['name'] ?? 'N/A',
                         'sold' => 0,
                         'profit_margin' => 0,
-                        'profit' => [
+                        'total' => 0,
+                        'profit' => 0,
+                        'profits' => [
                             '1d' => [],
                             '3d' => [],
                             '7d' => [],
@@ -83,30 +86,39 @@ class FetchSoldTicketsJob implements ShouldQueue
                 }
 
                 $events[$eventId]['sold'] += $quantity;
-                $events[$eventId]['profit_margin'] += $profit;
+                //get the profit margin
+                $totalCom = $item['total'];
+                $profitCom = $item['profit'];
+
+                $events[$eventId]['total'] += $totalCom;
+                $events[$eventId]['profit'] += $profitCom;
+
+                $events[$eventId]['profit_margin'] = ($events[$eventId]['total'] > 0) 
+                ? ($events[$eventId]['profit'] / $events[$eventId]['total']) * 100 
+                : 0;
 
                 // Categorize profits based on date ranges
                 if ($invoiceDate->between($oneDayAgo, $today)) {
-                    $events[$eventId]['profit']['1d'][] = $profit;
+                    $events[$eventId]['profits']['1d'][] = $profit;
                 }
                 if ($invoiceDate->between($threeDaysAgo, $today)) {
-                    $events[$eventId]['profit']['3d'][] = $profit;
+                    $events[$eventId]['profits']['3d'][] = $profit;
                 }
                 if ($invoiceDate->between($sevenDaysAgo, $today)) {
-                    $events[$eventId]['profit']['7d'][] = $profit;
+                    $events[$eventId]['profits']['7d'][] = $profit;
                 }
                 if ($invoiceDate->between($thirtyDaysAgo, $today)) {
-                    $events[$eventId]['profit']['30d'][] = $profit;
+                    $events[$eventId]['profits']['30d'][] = $profit;
                 }
             }
 
             $upsertData = [];
             // Calculate average profits
             foreach ($events as &$event) {
-                foreach ($event['profit'] as $key => $profits) {
+                foreach ($event['profits'] as $key => $profits) {
                     $event['avg_sold_' . $key] = !empty($profits) ? round(array_sum($profits) / count($profits), 2) : 0;
                 }
-                unset($event['profit']); // Remove intermediate profits array
+                unset($event['profits']); // Remove intermediate profits array
 
                 $upsertData[] = [
                     'event_id' => $event['event_id'],
@@ -115,7 +127,7 @@ class FetchSoldTicketsJob implements ShouldQueue
                     'venue' => $event['venue'],
                     'sold' => $event['sold'],
                     'qty' => $event['sold'],
-                    'profit_margin' => $event['profit_margin'],
+                    'profit_margin' => round($event['profit_margin'],2),
                     'avg_profit_1d' => $event['avg_sold_1d'],
                     'avg_profit_3d' => $event['avg_sold_3d'],
                     'avg_profit_7d' => $event['avg_sold_7d'],
@@ -132,6 +144,7 @@ class FetchSoldTicketsJob implements ShouldQueue
             $totalProfitMarginThisMonth = $totalQtyThisMonth > 0 ? $totalProfitThisMonth / $totalQtyThisMonth : 0;
 
             if (!empty($inventory)) {
+
                 Inventory::upsert($upsertData, ['event_id'], 
                     [
                         'name',
@@ -150,76 +163,10 @@ class FetchSoldTicketsJob implements ShouldQueue
             }
 
 
-            } catch (Exception $e) {
-                Log::error("FetchSoldTicketsJob encountered an error: " . $e->getMessage());
-            }
+        } catch (Exception $e) {
+            Log::error("FetchSoldTicketsJob encountered an error: " . $e->getMessage());
+        }
             //throw $th;
-        // try {
-        //     $apiToken = env('SKYBOX_API_TOKEN');
-        //     $authToken = env('SKYBOX_AUTH_TOKEN');
-        //     $soldTicketsData = [];
-
-        //     Inventory::whereYear('date', '>=', now()->subYear()->year)
-        //         ->chunk(50, function ($inventoryRecords) use ($authToken, $apiToken, &$soldTicketsData) {
-        //             foreach ($inventoryRecords as $inventory) {
-        //                 try {
-        //                     $eventId = $inventory->event_id;
-        //                     $soldTicketsUrl = "https://skybox.vividseats.com/services/inventory/sold?eventId={$eventId}";
-
-        //                     $soldResponse = Http::withHeaders([
-        //                         'X-Api-Token' => $authToken, 
-        //                         'X-Application-Token' => $apiToken,
-        //                         'Accept' => 'application/json',
-        //                     ])->get($soldTicketsUrl);
-
-        //                     if ($soldResponse->failed()) {
-        //                         Log::warning("FetchSoldTicketsJob failed for Event ID: {$eventId}");
-        //                         continue;
-        //                     }
-
-        //                     $soldData = $soldResponse->json();
-        //                     $soldQuantity = $soldData['soldInventoryTotals']['totalQuantity'] ?? 0;
-        //                     $totalProfitMargin = $soldData['soldInventoryTotals']['totalProfitMargin'] ?? 0;
-
-        //                     Inventory::where('event_id', $eventId)->update([
-        //                         'sold' => $soldQuantity,
-        //                         'profit_margin' => $totalProfitMargin,
-        //                         'updated_at' => now()
-        //                     ]);
-
-        //                     foreach ($soldData['rows'] ?? [] as $soldItem) {
-        //                         $soldTicketsData[] = [
-        //                             'event_id' => $eventId,
-        //                             'invoiceId' => $soldItem['invoiceId'] ?? 0,
-        //                             'customerDisplayName' => $soldItem['customerDisplayName'] ?? 0,
-        //                             'lowSeat' => $soldItem['lowSeat'] ?? 0,
-        //                             'highSeat' => $soldItem['highSeat'] ?? 0,
-        //                             'section' => $soldItem['section'] ?? 0,
-        //                             'cost' => $soldItem['cost'] ?? 0,
-        //                             'total' => $soldItem['total'] ?? 0,
-        //                             'profit' => $soldItem['profit'] ?? 0,
-        //                             'roi' => $soldItem['roi'] ?? 0,
-        //                             'invoiceDate' => isset($soldItem['invoiceDate']) 
-        //                                 ? Carbon::parse($soldItem['invoiceDate'])->format('Y-m-d H:i:s') 
-        //                                 : now(),
-        //                             'updated_at' => now(),
-        //                         ];
-        //                     }
-        //                 } catch (Exception $e) {
-        //                     Log::error("Error processing Event ID {$inventory->event_id}: " . $e->getMessage());
-        //                 }
-        //             }
-        //         });
-
-        //     if (!empty($soldTicketsData)) {
-        //         SoldTicket::upsert($soldTicketsData, ['invoiceId'], [
-        //             'event_id', 'customerDisplayName', 'lowSeat','highSeat', 'section', 'cost', 'total', 'profit', 'roi', 'invoiceDate', 'updated_at'
-        //         ]);
-        //     }
-
-        //     Log::info('FetchSoldTicketsJob completed successfully.');
-        // } catch (Exception $e) {
-        //     Log::error("FetchSoldTicketsJob encountered an error: " . $e->getMessage());
-        // }
+        
     }
 }
